c04d3fe8e58d2d65c859724483df8fb056e52a4a
leandrolfds <leandrolfds@hotmail.com>
2016-08-20 11:51:43 -0300

    versao02
    sd

--- plasma/js/jquery-loader.js
+++
@@ -1,8 +0,0 @@
-(function () {
-
-    // Get any lib=___ param from the query string.
-    var library = location.search.match(/[?&]lib=(.*?)(?=&|$)/);
-
-    document.write("<script src='../libs/" + library[1] + "'></script>");
-
-}());
